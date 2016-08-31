c97ced452a11a00a527c210ea8f8362b7c8d4686
Travis Wade <travis@entrision.com>
2016-08-30 19:01:56 -0700

    initial commit

---
+++ PunchCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PunchCard
+//
+//  Created by Travis Wade on 8/30/16.
+//  Copyright © 2016 Branvis Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PunchCard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PunchCard
+//
+//  Created by Travis Wade on 8/30/16.
+//  Copyright © 2016 Branvis Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
